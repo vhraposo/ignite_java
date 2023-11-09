9c8175f54e47b4827460871ccc93bb81e8e375ca
vhraposo <thevhup@gmail.com>
2023-11-08 23:33:44 -0300

    Aula: Component Scan

--- primeiro-projeto-springboot/src/main/java/br/com/vraposo/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplication.java
+++ primeiro-projeto-springboot/src/main/java/br/com/vraposo/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplication.java
@@ -2,8 +2,10 @@ package br.com.vraposo.primeiroprojetospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages =  "br.com.vraposo")
 public class PrimeiroProjetoSpringbootApplication {
 
 	public static void main(String[] args) {
